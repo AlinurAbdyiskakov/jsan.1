6beed459a6d87dbda7d0bd185256be639943dfd5
AlinurAbdyiskakov <abdyiskakovalinur7>
2021-02-18 14:36:55 +0600

    add new objact file

--- scripts.js
+++ scripts.js
@@ -1,16 +1,35 @@
-let ul = document.createElement('ul');
-document.body.append(ul);
+
+let table = document.createElement('table');
+document.body.append(table);
+
+let tr = document.createElement('tr');
+let tdName = document.createElement('th');
+let tdAge = document.createElement('th');
+let tdWealth = document.createElement('th');
+tdName.innerText = "Name";
+tdWealth.innerText = "Wealth";
+tdAge.innerText = "Age";
+tr.append(tdName);
+tr.append(tdAge);
+tr.append(tdWealth);
+table.append(tr);
+
 fetch("./data.json")
-.then(function(response){
-  return response.json();
-})
-.then(function(data){
- for (const key in data) {
-    let li = document.createElement('li');
-    li.innerText = data[key].name + "-" + "$" + data[key].wealth;
-    ul.append(li);
-}
-})
-
-
-
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        for (const key in data) {
+            let tr = document.createElement('tr');
+            let tdName = document.createElement('td');
+            let tdAge = document.createElement('td');
+            let tdWealth = document.createElement('td');
+            tdName.innerText = data[key].name;
+            tdWealth.innerText = "$" + data[key].wealth;
+            tdAge.innerText = data[key].age;
+            tr.append(tdName);
+            tr.append(tdAge);
+            tr.append(tdWealth);
+            table.append(tr);
+        }
+    });
