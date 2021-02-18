755b45e76545fab8dfa7c088abd4076db44dd6c4
AlinurAbdyiskakov <abdyiskakovalinur7>
2021-02-18 14:49:21 +0600

    add new stylization scripts.js

--- scripts.js
+++ scripts.js
@@ -3,12 +3,16 @@ let table = document.createElement('table');
 document.body.append(table);
 
 let tr = document.createElement('tr');
+let strin = document.createElement('th');
 let tdName = document.createElement('th');
 let tdAge = document.createElement('th');
 let tdWealth = document.createElement('th');
+
 tdName.innerText = "Name";
 tdWealth.innerText = "Wealth";
 tdAge.innerText = "Age";
+strin.innerText = "ID"
+tr.append(strin)
 tr.append(tdName);
 tr.append(tdAge);
 tr.append(tdWealth);
@@ -21,15 +25,21 @@ fetch("./data.json")
     .then(function (data) {
         for (const key in data) {
             let tr = document.createElement('tr');
+            let  strin = document.createElement('td');
+            
             let tdName = document.createElement('td');
             let tdAge = document.createElement('td');
             let tdWealth = document.createElement('td');
+           
+            strin.innerText = key;
             tdName.innerText = data[key].name;
             tdWealth.innerText = "$" + data[key].wealth;
             tdAge.innerText = data[key].age;
-            tr.append(tdName);
+            tr.append(strin);
+             tr.append(tdName);
             tr.append(tdAge);
             tr.append(tdWealth);
+           
             table.append(tr);
         }
     });
